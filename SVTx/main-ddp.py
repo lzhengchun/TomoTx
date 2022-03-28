8d9458f6f3bb48c887e1c30ffa789b0f3a8546f7
Zhengchun Liu <zhengchun.liu@anl.gov>
2022-03-28 10:55:34 -0500

    baby-step update

---
+++ SVTx/main-ddp.py
@@ -0,0 +1,121 @@
+#! /homes/zhengchun.liu/usr/miniconda3/envs/hvd/bin/python
+
+# torchrun --standalone --nnodes=1 --nproc_per_node=8  ./main-ddp.py -cfg=config/simu.yaml -expName=simu
+
+from model import SVTx
+import torch, argparse, os, time, sys, shutil, yaml
+from util import save2img, restore_and_save2tiff
+from data import SinogramDataset
+import numpy as np
+import logging
+from torch.utils.data import DataLoader
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+parser = argparse.ArgumentParser(description='SVTx')
+parser.add_argument('-gpus',   type=str, default="", help='list of visiable GPUs')
+parser.add_argument('-expName',type=str, default="debug", help='Experiment name')
+parser.add_argument('-cfg',    type=str, required=True, help='path to config yaml file')
+parser.add_argument('-verbose',type=int, default=1, help='1:print to terminal; 0: redirect to file')
+
+def main(args):
+    local_rank = int(os.environ["LOCAL_RANK"])
+    rank = int(os.environ["RANK"])
+    world_size = int(os.environ["WORLD_SIZE"])
+
+    if torch.distributed.is_nccl_available():
+        torch.distributed.init_process_group("nccl", rank=rank, world_size=world_size)
+    else:
+        torch.distributed.init_process_group("gloo", rank=rank, world_size=world_size)
+
+    itr_out_dir = args.expName + '-itrOut'
+    if rank == 0:
+        if os.path.isdir(itr_out_dir):
+            shutil.rmtree(itr_out_dir)
+        os.mkdir(itr_out_dir) # to save temp output
+    torch.distributed.barrier()
+
+    logging.basicConfig(filename=f"{args.expName}-itrOut/SVTx.log", level=logging.DEBUG)
+    if args.verbose:
+        logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+    # training task init
+    params = yaml.load(open(args.cfg, 'r'), Loader=yaml.CLoader)
+    logging.info(f"local rank {local_rank} (global rank {rank}) of a world size {world_size} started")
+
+    torch.cuda.set_device(local_rank)
+
+    logging.info(f"rank {rank} Init training dataset ...")
+    train_ds = SinogramDataset(ifn=params['dataset']['th5'], params=params, world_size=world_size, rank=rank)
+    train_dl = DataLoader(train_ds, batch_size=params['train']['mbsz']//world_size, num_workers=4, \
+                          prefetch_factor=params['train']['mbsz'], pin_memory=True, \
+                          drop_last=True, shuffle=True)
+    logging.info(f"rank {rank} %d samples, {train_ds.shape}, will be used for training" % (len(train_ds), ))
+
+    if rank == world_size-1: 
+        logging.info("Init validation dataset ...")
+        valid_ds = SinogramDataset(ifn=params['dataset']['vh5'], params=params)
+        valid_dl = DataLoader(valid_ds, batch_size=params['train']['mbsz']//world_size, num_workers=4, \
+                              prefetch_factor=params['train']['mbsz'], pin_memory=True, \
+                              drop_last=False, shuffle=False)
+        logging.info(f"%d samples, {valid_ds.shape},  will be used for validation" % (len(valid_ds), ))
+
+    model = SVTx(seqlen=train_ds.seqlen, in_dim=train_ds.cdim, params=params).cuda()
+    model = DDP(model, device_ids=[local_rank], output_device=local_rank, find_unused_parameters=False)
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=params['train']['lr'], betas=(0.9, 0.95))
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.4, verbose=rank==0)
+
+    logging.info(f"rank {rank} Start training ...")
+    for ep in range(1, params['train']['maxep']+1):
+        train_ep_tick = time.time()
+        for imgs_tr in train_dl: 
+            optimizer.zero_grad()
+            loss, pred, mask = model.forward(imgs_tr.cuda())
+            loss.backward()
+            optimizer.step()
+
+        lr_scheduler.step()
+        if rank != world_size-1: continue
+
+        time_e2e = time.time() - train_ep_tick
+        itr_prints = '[Train] Epoch %3d, loss: %.6f, elapse: %.2fs/epoch, %d steps with lbs=%d' % (\
+                     ep, loss.cpu().detach().numpy(), time_e2e, len(train_dl), imgs_tr.shape[0])
+        logging.info(itr_prints)
+
+        val_loss = []
+        valid_ep_tick = time.time()
+        for imgs_val in valid_dl:
+            with torch.no_grad():
+                _vloss, _vpred, _vmask = model.forward(imgs_val.cuda())
+                val_loss.append(_vloss.cpu().numpy())
+
+        valid_e2e = time.time() - valid_ep_tick
+        _prints = '[Valid] Epoch %3d, loss: %.6f, elapse: %.2fs/epoch\n' % (ep, np.mean(val_loss), valid_e2e)
+        logging.info(_prints)
+
+        if ep % params['train']['ckp_steps'] != 0: continue
+
+        save2img(imgs_val[-1].numpy().squeeze(), '%s/ep%05d-valid-gt.tiff' % (itr_out_dir, ep))
+        restore_and_save2tiff(pred=_vpred[-1].cpu().numpy().squeeze(), ori=imgs_val[-1].numpy().squeeze(), \
+                              mask=_vmask[-1].cpu().numpy(), fn='%s/ep%05d-valid-pd.tiff' % (itr_out_dir, ep))
+
+        save2img(imgs_tr[-1].numpy().squeeze(), '%s/ep%05d-train-gt.tiff' % (itr_out_dir, ep))
+        restore_and_save2tiff(pred=pred[-1].detach().cpu().numpy().squeeze(), ori=imgs_tr[-1].numpy().squeeze(), \
+                              mask=mask[-1].cpu().numpy(), fn='%s/ep%05d-train-pd.tiff' % (itr_out_dir, ep))
+
+        torch.save(model.module.state_dict(), "%s/mdl-ep%05d.pth" % (itr_out_dir, ep))
+        # torch.jit.save(torch.jit.trace(model, (imgs_tr[:1].cuda(), 0.7)), "%s/script-ep%05d.pth" % (itr_out_dir, ep))
+        with open(f'{itr_out_dir}/config.yaml', 'w') as fp:
+            yaml.dump(params, fp)
+
+if __name__ == "__main__":
+    args, unparsed = parser.parse_known_args()
+
+    if len(unparsed) > 0:
+        print('Unrecognized argument(s): \n%s \nProgram exiting ... ... ' % '\n'.join(unparsed))
+        exit(0)
+
+    if len(args.gpus) > 0:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
+
+    main(args)
---
+++ SVTx/transformer.py
@@ -0,0 +1,109 @@
+# --------------------------------------------------------
+# building blocks for Transformer
+# References: https://github.com/rwightman/pytorch-image-models
+# --------------------------------------------------------
+
+import torch
+from itertools import repeat
+import collections.abc
+
+# From PyTorch internals
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, collections.abc.Iterable):
+            return x
+        return tuple(repeat(x, n))
+    return parse
+
+class Mlp(torch.nn.Module):
+    """
+    MLP as used in Vision Transformer, MLP-Mixer and related networks
+    """
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=torch.nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        drop_probs = _ntuple(2)(drop)
+
+        self.fc1 = torch.nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.drop1 = torch.nn.Dropout(drop_probs[0])
+        self.fc2 = torch.nn.Linear(hidden_features, out_features)
+        self.drop2 = torch.nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+
+def drop_path(x, drop_prob: float = 0., training: bool = False, scale_by_keep: bool = True):
+    """
+    Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+    if keep_prob > 0.0 and scale_by_keep:
+        random_tensor.div_(keep_prob)
+    return x * random_tensor
+
+class DropPath(torch.nn.Module):
+    """
+    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    """
+    def __init__(self, drop_prob=None, scale_by_keep=True):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+        self.scale_by_keep = scale_by_keep
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training, self.scale_by_keep)
+
+class Attention(torch.nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        assert dim % num_heads == 0, 'dim should be divisible by num_heads'
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+
+        self.qkv = torch.nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = torch.nn.Dropout(attn_drop)
+        self.proj = torch.nn.Linear(dim, dim)
+        self.proj_drop = torch.nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)   # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+class Block(torch.nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=torch.nn.GELU, norm_layer=torch.nn.LayerNorm):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else torch.nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
